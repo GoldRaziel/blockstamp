5d01c7fd09ded6d5162379f41a874f0b9e439f23
GoldRaziel <paolocimino21@gmail.com>
2025-08-28 15:38:49 +0000

    fix(i18n): rimosso i18n dal next.config; manteniamo solo redirect Netlify per default EN

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** Auto-generated to unblock Netlify build. */
 module.exports = {
-  i18n: {
-    locales: ["en", "it", "ar"],
-    defaultLocale: "en",
-  },
 
   "typescript": {
     "ignoreBuildErrors": true
